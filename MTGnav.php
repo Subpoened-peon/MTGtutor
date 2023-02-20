234eb22e718188b97c5f1e1ca24d70054bbc6f81
Subpoened-peon <www.nic.merritt@gmail.com>
2023-02-19 20:05:36 -0700

    Added dropdown

---
+++ MTGnav.php
@@ -0,0 +1,17 @@
+<html>
+
+    <head>
+     <link rel="stylesheet" href="MTGstyles.css">
+    </head>
+
+    <body>
+        <form id ="L" method= "post">
+            <select name= "Page">
+                <option value="MTGtutor/MTGtutorHome">HOME</option>
+                <option value="news">NEWS</option>
+            </select>
+            <input type ="submit" name="GO" value="GO">
+        </form>
+    </body>
+
+</html>
