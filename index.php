6f615bd6e33495773c01b021235351a6c60a8103
Nicholas Collin Merritt <107739492+Subpoened-peon@users.noreply.github.com>
2023-02-23 00:42:20 -0700

    testing 4

--- index.php
+++ index.php
@@ -1,1 +1,42 @@
-<?php require_once("MTGtutorhome.php"); ?>
+<?php require_once("MTGnav.php"); ?>
+
+<html> 
+ <head>
+ <link rel="icon" type="image/x-icon" href="demfavicon.ico">
+</head>
+<body style="background-color: grey;">
+
+    <div class="background">
+    <img src= "MTGTUTOR.jpg" class= "img1"/>
+        <h1 id= "title">
+     MTGtutor
+        </h1>
+    </div>
+    
+    <div>
+    <form id="text">
+    <label for= "search">
+        <a href = "MTGadvanced.php">Advanced</a></label>
+    <input type= "text" name="search" placeholder= "Search your library for...">
+</form>
+    </div>
+
+
+    <div>
+       <p class = "content1"> Recently Viewed 
+        <img src = "gob.jpg" />
+        <img src = "domcon.jpg" />
+       </p>
+
+       <p class = "content1"> Recently Added
+        <img src = "gob.jpg" />
+        <img src = "domcon.jpg" />
+       </p>
+       
+    </div>
+
+    
+</body>
+</html>
+
+<?php include("footer.php"); ?>
